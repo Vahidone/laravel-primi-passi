1db76a68cefff2d86ae56c0ae2494c10c3b40e1e
Vahidone <v.ban.roma@gmail.com>
2023-11-16 15:51:06 +0100

    feat: add buttons to back to home

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -13,6 +13,12 @@
 <body>
     <div class="container text-center mt-5 pt-5">
         <h1 class="text-success fw-bold ">{{ $text }}</h1>
+
+
+        <div class="mt-5">
+            <button class="btn btn-primary fs-3"><a class="text-white text-decoration-none" href="/">Torna</a></button>
+        </div>
+
     </div>
 
 </body>
